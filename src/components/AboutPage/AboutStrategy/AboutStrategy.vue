300128dfdde61ef81a04b2378946a7b53b002637
David Ashikyan <dashikyandav@mail.ru>
2021-09-06 20:07:38 +0800

    make about strategy page

--- src/components/AboutPage/AboutStrategy/AboutStrategy.vue
+++ src/components/AboutPage/AboutStrategy/AboutStrategy.vue
@@ -1,11 +1,14 @@
 <template>
+  <ShowPathPage :page="{path:'About Us',section:'Strategy'}"/>
   <ShowBackgroundImage img="strategy" :text="{title:'Our strategy', text:'Our strategy supports our ambition of being the preferred international financial partner for our clients.' }" />
 </template>
 
 <script>
 import ShowBackgroundImage from "../../ui/ShowBackgroundImage";
+import ShowPathPage from "../../ui/ShowPathPage";
 export default {
   components: {
+    ShowPathPage,
     ShowBackgroundImage
 
   },
